ac845b1820882820dad1dbc2d867e78b74d20461
akash_bhardwaj <akashbhardwaj2866@gmail.com>
2023-12-12 17:28:06 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,22 @@
+
+var randomNumber1 = Math.round(Math.random() * 5) + 1;
+
+var randomNumber2 = Math.round(Math.random() * 5) + 1;
+
+var image1 = "./images/dice" + randomNumber1 + ".png";
+
+var image2 = "./images/dice" + randomNumber2 + ".png";
+
+document.querySelector("img.img1").setAttribute("src", image1);
+
+document.querySelector("img.img2").setAttribute("src", image2);
+
+if(randomNumber1>randomNumber2){
+    document.querySelector("h1").innerHTML="Player 1 Wins!";
+}
+else if(randomNumber1<randomNumber2){
+    document.querySelector("h1").innerHTML="Player 2 Wins!";
+}
+else{
+    document.querySelector("h1").innerHTML="Draw!";
+}
